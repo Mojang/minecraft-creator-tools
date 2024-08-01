5e8eec0da3454e2493b03b34ffb771afb1cb5410
Mike Ammerlaan <mammerla@users.noreply.github.com>
2024-08-01 06:09:57 -0700

    Update version number and add a test

--- app/src/test/CommandLineTest.ts
+++ app/src/test/CommandLineTest.ts
@@ -235,6 +235,50 @@ describe("validateAddons3ExtraneousStuffUsesMinecraftCommand", async () => {
   });
 });
 
+describe("validateAddons3PlatformVersions", async () => {
+  let exitCode: number | null = null;
+  const stdoutLines: string[] = [];
+  const stderrLines: string[] = [];
+
+  before(function (done) {
+    this.timeout(20000);
+
+    removeResultFolder("validateAddons3PlatformVersions");
+
+    const process = spawn("node", [
+      " ./../toolbuild/jsn/cli",
+      "val",
+      "currentplatform",
+      "-i",
+      "./../samplecontent/addon/build/content3",
+      "-o",
+      "./test/results/validateAddons3PlatformVersions/",
+    ]);
+
+    collectLines(process.stdout, stdoutLines);
+    collectLines(process.stderr, stderrLines);
+
+    process.on("exit", (code) => {
+      exitCode = code;
+      done();
+    });
+  });
+
+  it("should have no stderr lines", async (done) => {
+    assert.equal(stderrLines.length, 0, "Error: |" + stderrLines.join("\n") + "|");
+    done();
+  }).timeout(10000);
+
+  it("exit code should be zero", async (done) => {
+    assert.equal(exitCode, 0);
+    done();
+  }).timeout(10000);
+
+  it("output matches", async function () {
+    await folderMatches("validateAddons3PlatformVersions");
+  });
+});
+
 async function collectLines(readable: Readable, data: string[]) {
   for await (const line of chunksToLinesAsync(readable)) {
     if (line !== undefined && line.length >= 0) {
