03223251459f88b519f9391d36f50779fdc2ab74
Mike Ammerlaan <mammerla@users.noreply.github.com>
2026-04-01 16:03:51 -0700

    fix: Use a subset script for release builds (#110)
    * fix: Use a subset script for release builds
    
    * fix: Use base semantic release versions

--- app/src/core/Constants.ts
+++ app/src/core/Constants.ts
@@ -1,5 +1,5 @@
 export const constants = {
-  version: "0.2.9",
+  version: "0.0.0-semantically-released",
   name: "Minecraft Creator Tools",
   disclaimer: "Alpha software. License at https://github.com/mojang/minecraft-creator-tools.",
   copyright: "Copyright 2026 Mojang AB. All rights reserved.",
